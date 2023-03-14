058c3fbbef7f75024470baf653a9063833c72ea3
salmaqnsGH <nurussalamahqonaah@gmail.com>
2023-03-14 16:21:49 +0700

    refactor looping for i value

--- challenge2/main.go
+++ challenge2/main.go
@@ -5,17 +5,19 @@ import "fmt"
 func main() {
 	arr := []rune{'С', 'А', 'Ш', 'А', 'Р', 'В', 'О'}
 
-	for j := 0; j <= 10; j++ {
-		if j == 0 {
-			for i := 0; i <= 4; i++ {
-				fmt.Println("Nilai i =", i)
+	for i := 0; i <= 10; i++ {
+		fmt.Println("Nilai i =", i)
+		if i == 4 {
+			for j := 0; j <= 10; j++ {
+				if j == 5 {
+					for i, v := range arr {
+						fmt.Printf("character %U '%c' starts at byte position %d \n", v, v, i*2)
+					}
+					continue
+				}
+				fmt.Println("Nilai j =", j)
 			}
-		} else if j == 5 {
-			for i, v := range arr {
-				fmt.Printf("character %U '%c' starts at byte position %d \n", v, v, i*2)
-			}
-			continue
+			break
 		}
-		fmt.Println("Nilai j =", j)
 	}
 }
