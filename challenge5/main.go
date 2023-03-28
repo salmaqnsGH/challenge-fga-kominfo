ac73649754e8d56aafb47f50935ffd6a4c534bbc
salmaqnsGH <nurussalamahqonaah@gmail.com>
2023-03-28 14:10:07 +0700

    connected to db

--- challenge5/main.go
+++ challenge5/main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
-	router "belajar-gin/routers"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
 )
 
-const PORT = ":4000"
+const (
+	PORT        = ":4000"
+	DB_HOST     = "localhost"
+	DB_USER     = "root"
+	DB_PASSWORD = "secret"
+	DB_PORT     = 5432
+	DB_NAME     = "book"
+)
 
 func main() {
-	router.StartServer().Run(PORT)
+	// router.StartServer().Run(PORT)
+
+	connStr := fmt.Sprintf("user=%s dbname=%s password=%s host=%s port=%d sslmode=disable", DB_USER, DB_NAME, DB_PASSWORD, DB_HOST, DB_PORT)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected to database!")
 }
